0bd4f558f31b2ecbc7bca127db45ed67643342b8
heekki2024 <heekki2024@gmail.com>
2024-07-25 18:00:42 +0900

    8

--- automitm.py
+++ automitm.py
@@ -93,47 +93,60 @@ def pull_apks(apk_files, package_dir, package_name):
             print(f"파일 추출 실패")
             raise e
         
+
 def merge_apks(package_dir):
     try:
-        print("apk들을 merge 합니다...")
-        subprocess.run(['java', '-jar', r'C:\Windows\APKEditor-1.3.8.jar', 'm', '-i', package_dir , '-o', ], check=True)
 
+        merged_apk_path = os.path.join(f"{package_dir}2", "merged.apk")
 
-def decompile_base_apk(package_dir):
+        print("apk들을 Merge 합니다...")
+        subprocess.run(['java', '-jar', r'C:\Windows\APKEditor-1.3.8.jar', 'm', '-i', package_dir , '-o', merged_apk_path], check=True)
+        print("MERGE가 완료되었습니다.")
+        return merged_apk_path
+    except subprocess.CalledProcessError as e:
+        print(f"파일 MERGE 실패")
+        raise e
+
+def decompile_merged_apk(merged_apk_path, package_dir):
     try:
-        base_apk_path = os.path.join(package_dir, "base.apk")
-        print(base_apk_path)
-        output_path = f"{package_dir}2"
-        print(output_path)
-        subprocess.run(['java', '-jar', 'C:\\Windows\\apktool.jar', 'd', '-f', '-s', base_apk_path, '-o', output_path], check=True)
-        print(f"base.apk 디컴파일 완료: {output_path}")
+        # base_apk_path = os.path.join(package_dir, "base.apk")
+        # print(base_apk_path)
+        print("r 옵션 없이 디컴파일 하는 중 ...")
+        output_path = f"{package_dir}3"
+        # print(output_path)
+        subprocess.run(['java', '-jar', 'C:\\Windows\\apktool.jar', 'd', '-f', '-s', merged_apk_path, '-o', output_path], check=True)
+        print(f"merged 디컴파일 완료: {output_path}")
         return output_path
     except subprocess.CalledProcessError as e:
-        print(f"{base_apk_path} 디컴파일 중 오류 발생: {e}")
+        print(f"{merged_apk_path} 디컴파일 중 오류 발생: {e}")
         raise e
     except Exception as e:
-        print(f"{base_apk_path} 파일을 찾을 수 없습니다.")
+        print(f"{merged_apk_path} 파일을 찾을 수 없습니다.")
         raise e
 
-def decompile_base_apk_with_r(package_dir):
+def decompile_merged_apk_with_r(merged_apk_path, package_dir):
     try:
-        base_apk_path = os.path.join(package_dir, "base.apk")
-        print(base_apk_path)
-        output_path = f"{package_dir}2"
-        print(output_path)
-        subprocess.run(['java', '-jar', 'C:\\Windows\\apktool.jar', 'd', '-f', '-r' '-s', base_apk_path, '-o', output_path], check=True)
-        print(f"base.apk 디컴파일 완료: {output_path}")
+        # base_apk_path = os.path.join(package_dir, "base.apk")
+        # print(base_apk_path)
+        print("r 옵션이 있는 상태로 디컴파일 하는 중 ...")
+        output_path = f"{package_dir}3"
+        # print(output_path)
+        subprocess.run(['java', '-jar', 'C:\\Windows\\apktool.jar', 'd', '-f', '-r', '-s', merged_apk_path, '-o', output_path], check=True)
+        print(f"merged.apk 디컴파일 완료: {output_path}")
         return output_path
     except subprocess.CalledProcessError as e:
-        print(f"{base_apk_path} 디컴파일 중 오류 발생: {e}")
+        print(f"{merged_apk_path} 디컴파일 중 오류 발생: {e}")
         raise e
     except Exception as e:
-        print(f"{base_apk_path} 파일을 찾을 수 없습니다.")
+        print(f"{merged_apk_path} 파일을 찾을 수 없습니다.")
         raise e
 
 def check_AndroidManifest(output_path):
     try:
         AndroidManifest_path = os.path.join(output_path, "AndroidManifest.xml")
+
+        if not os.path.exists(AndroidManifest_path):
+            raise FileNotFoundError(f"{AndroidManifest_path} 파일을 찾을 수 없습니다.")
 
         # AndroidManifest.xml 파일 파싱
         tree = ET.parse(AndroidManifest_path)
@@ -182,27 +195,63 @@ def check_AndroidManifest(output_path):
             print("android:networkSecurityConfig 옵션이 추가되었습니다.")
 
             tree.write(AndroidManifest_path, encoding='utf-8', xml_declaration=True)
-
-            return False
-
-
+            print("AndroidManifest.xml 저장 완료")
+            return False,None
 
 
     except ET.ParseError as e:
         print(f"XML 파싱 오류: {e}")
+        raise e
+    
     except FileNotFoundError as e:
         print(f"파일을 찾을 수 없습니다: {e}")
+        raise e
+
     except Exception as e:
         print(f"알 수 없는 오류: {e}")
+        raise e
 
 
-        if not os.path.exists(AndroidManifest_path):
-            raise FileNotFoundError(f"{AndroidManifest_path} 파일을 찾을 수 없습니다.")
-
-        
-            
 
 
+def modify_network_security_config(network_Security_Config_name, output_path):
+    try:
+        network_Security_Config_name_path = os.path.join(output_path, "res", 'xml', f"{network_Security_Config_name}.xml")
+
+        # network_security_config.xml 파일 파싱
+        tree = ET.parse(network_Security_Config_name_path)
+        root = tree.getroot()
+
+        # 새로운 base-config 태그 내용
+        new_base_config = ET.Element('base-config', {'cleartextTrafficPermitted': 'true'})
+        trust_anchors = ET.SubElement(new_base_config, 'trust-anchors')
+        ET.SubElement(trust_anchors, 'certificates', {'src': 'system'})
+        ET.SubElement(trust_anchors, 'certificates', {'src': 'user'})
+
+        # 기존 base-config 태그 찾기
+        base_config = root.find('base-config')
+
+        if base_config is not None:
+            # base-config 태그와 내용 제거
+            root.remove(base_config)
+
+        # 새로운 base-config 태그 추가
+        root.append(new_base_config)
+
+        # 변경된 내용 저장
+        tree.write(network_Security_Config_name_path, encoding='utf-8', xml_declaration=True)
+        print(f"{network_Security_Config_name_path} 파일이 수정되었습니다.")
+
+    except ET.ParseError as e:
+        print(f"XML 파싱 오류: {e}")
+        raise e
+    except FileNotFoundError as e:
+        print(f"파일을 찾을 수 없습니다: {e}")
+        raise e
+    except Exception as e:
+        print(f"알 수 없는 오류: {e}")
+        raise e
+         
 def copy_network_security_config(output_path):
     try:
         network_security_config_dir = os.path.join(output_path, "res", "xml")
@@ -213,7 +262,7 @@ def copy_network_security_config(output_path):
 
         # 파일 존재 여부 확인
         if not os.path.exists(network_security_config_dir):
-            raise FileNotFoundError(f"res\xml 폴더를 찾을 수 없습니다.")
+            raise FileNotFoundError(r"res\xml 폴더를 찾을 수 없습니다.")
         
         if not os.path.exists(modified_network_security_config_path):
             raise FileNotFoundError(f"변조된 network_security_config 파일을 찾을 수 없습니다.")
@@ -231,25 +280,23 @@ def copy_network_security_config(output_path):
         print("유효하지 않은 output_path입니다.")
         raise e
 
-def change_network_security_config_with_r(output_path):
+def copy_network_security_config_with_r(output_path):
     try:
-        network_security_config_path = os.path.join(output_path, "res", "xml", "network_security_config.xml")
+        network_security_config_dir = os.path.join(output_path, "res", "xml")
 
         #변조된 파일 경로
-        # modified_network_security_config_path = r"C:\Users\BSJ\Desktop\network_security_config_without_r\network_security_config.xml"
+        # modified_network_security_config_path = r"C:\Users\BSJ\Desktop\network_security_config\network_security_config.xml"
         modified_network_security_config_path = r"C:\Users\xten\Desktop\network_security_config_with_r\network_security_config.xml"
 
         # 파일 존재 여부 확인
-        if not os.path.exists(network_security_config_path):
-            raise FileNotFoundError(f"{network_security_config_path} 파일을 찾을 수 없습니다.")
+        if not os.path.exists(network_security_config_dir):
+            raise FileNotFoundError(r"res\xml 폴더를 찾을 수 없습니다.")
         
-
         if not os.path.exists(modified_network_security_config_path):
-            raise FileNotFoundError(f"{modified_network_security_config_path} 파일을 찾을 수 없습니다.")
-
-
-        shutil.copy(modified_network_security_config_path, network_security_config_path)
-        print(f"network_security_config.xml 파일이 교체되었습니다")
+            raise FileNotFoundError(f"변조된 network_security_config 파일을 찾을 수 없습니다.")
+ 
+        shutil.copy2(modified_network_security_config_path, network_security_config_dir)
+        print(f"network_security_config.xml 파일이 {network_security_config_dir}로 복사되었습니다.")
     except PermissionError as e:
         print(f"{modified_network_security_config_path} 파일 복사 중 오류 발생: {e}")
         raise e
@@ -260,7 +307,6 @@ def change_network_security_config_with_r(output_path):
         print("유효하지 않은 output_path입니다.")
         raise e
 
-
 def remove_decompile_fail_output_path(output_path):
     try:
         os.remove(output_path)
@@ -269,10 +315,10 @@ def remove_decompile_fail_output_path(output_path):
         raise e
         
 
-def recompile_base_apk(output_path, package_dir):
+def recompile_merged_apk(output_path, package_dir):
     try:
-        newbase_path = os.path.join(package_dir, "newbase.apk")
-        subprocess.run(['java', '-jar', 'C:\\Windows\\apktool.jar', 'b', output_path, '-o', newbase_path], check=True)
+        new_merged_path = os.path.join(package_dir, "new_merged.apk")
+        subprocess.run(['java', '-jar', 'C:\\Windows\\apktool.jar', 'b', output_path, '-o', new_merged_path], check=True)
         print("리컴파일에 성공하였습니다.")
     except subprocess.CalledProcessError as e:
         print("리컴파일에 실패했습니다. 프로그램을 종료합니다.")
@@ -289,49 +335,51 @@ def recompile_base_apk(output_path, package_dir):
 
         raise e
     # 리컴파일 완료 후 base.apk 파일 삭제
-    try:
-        base_apk_path = os.path.join(package_dir, "base.apk")
-        os.remove(base_apk_path)
-        print(f"{base_apk_path} 파일이 삭제되었습니다.")
-    except FileNotFoundError as e:
-        print(f"{base_apk_path} 파일을 찾지 못하였습니다: {e}")
-        raise e
-    except Exception as e:
-        print(f"{base_apk_path} 파일 삭제 중 오류 발생: {e}")
-        raise e
+    # try:
+    #     base_apk_path = os.path.join(package_dir, "base.apk")
+    #     os.remove(base_apk_path)
+    #     print(f"{base_apk_path} 파일이 삭제되었습니다.")
+    # except FileNotFoundError as e:
+    #     print(f"{base_apk_path} 파일을 찾지 못하였습니다: {e}")
+    #     raise e
+    # except Exception as e:
+    #     print(f"{base_apk_path} 파일 삭제 중 오류 발생: {e}")
+    #     raise e
 
 
 def sign_apks(package_dir):
-    for file in os.listdir(package_dir):
-        if file.endswith(".apk"):
-            apk_path = os.path.join(package_dir, file)
+    # for file in os.listdir(package_dir):
+    #     if file.endswith(".apk"):
+            built_apk_path = os.path.join(package_dir, "new_merged.apk")
             try:
-                subprocess.run(['java', '-jar', 'C:\\Windows\\uber-apk-signer-1.3.0.jar', '-a', apk_path, '--allowResign'], check=True)
-                print(f"{file} 파일 서명 완료.")
+                subprocess.run(['java', '-jar', 'C:\\Windows\\uber-apk-signer-1.3.0.jar', '-a', built_apk_path, '--allowResign'], check=True)
+                print(f"new_merged.apk 파일 서명 완료.")
             except subprocess.CalledProcessError as e:
-                print(f"{file} 파일 서명 실패.")
+                print(f"new_merged.apk 파일 서명 실패.")
 
                 global error
                 error = '서명 실패'
         
-
                 raise e
 
-def install_signed_apks(package_name, package_dir):
+def remove_app(package_name):
     # 기존 패키지 삭제
     try:
         subprocess.run(['adb', 'uninstall', package_name], check=True)
         print(f"{package_name} 패키지 삭제 완료.")
     except subprocess.CalledProcessError as e:
         print(f"{package_name} 패키지 삭제 실패.")
-        raise e
+        raise e    
+
+def install_signed_apks(package_dir):
+
     # 서명된 APK 파일 설치
     apk_files_to_install = []
     for file in os.listdir(package_dir):
         if file.endswith("-aligned-debugSigned.apk") and not file.endswith(".idsig"):
             apk_files_to_install.append(os.path.join(package_dir, file))
     try:
-        subprocess.run(['adb', 'install-multiple'] + apk_files_to_install, check=True)
+        subprocess.run(['adb', 'install'] + apk_files_to_install, check=True)
         print("서명된 APK 파일 설치 완료.")
     except subprocess.CalledProcessError as e:
         print("서명된 APK 파일 설치 실패.")
@@ -362,6 +410,12 @@ def main():
             app_name = ws[f'A{row}'].value
             package_name = ws[f'B{row}'].value
 
+            # 값 검증
+            if not app_name:
+                raise ValueError(f"행 {row}에서 app_name이 비어있거나 유효하지 않습니다.")
+            if not package_name:
+                raise ValueError(f"행 {row}에서 package_name이 비어있거나 유효하지 않습니다.")
+
             # APK 경로 가져오기
             apk_files = get_apk_paths(package_name)
 
@@ -371,40 +425,45 @@ def main():
             # APK 파일 추출
             pull_apks(apk_files, package_dir, package_name)
 
+            # APK 파일들 MERGE
+            merged_apk_path = merge_apks(package_dir)
+
             # base.apk 파일 디컴파일
-            output_path = decompile_base_apk(package_dir)
+            output_path = decompile_merged_apk(merged_apk_path, package_dir)
 
             have_networkSecurityConfig,network_Security_Config_name = check_AndroidManifest(output_path)
 
             if have_networkSecurityConfig == True:
+                #network_security_config.xml 내용 수정
+                modify_network_security_config(network_Security_Config_name, output_path)
                 
+                recompile_merged_apk(output_path, package_dir)
+
             elif have_networkSecurityConfig == False:
                 # network_security_config.xml 파일 교체
                 copy_network_security_config(output_path)
+                try:
+                    recompile_merged_apk(output_path, package_dir)
+                except:
+                    print("--------리컴파일 실패-------- \n디컴파일 된 폴더 삭제 후\n apktool d 의 -r 옵션 없이 재디컴파일")
 
-                recompile_base_apk(output_path, package_dir)
+                    remove_decompile_fail_output_path(output_path)
 
+                    output_path = decompile_merged_apk_with_r(merged_apk_path)
 
+                    copy_network_security_config_with_r(output_path)
 
-            try:
-                # base.apk 파일 리컴파일
-                recompile_base_apk(output_path, package_dir)
-            except:
-                print("--------리컴파일 실패-------- \n디컴파일 된 폴더 삭제 후\n apktool d 의 -r 옵션 없이 재디컴파일")
-                remove_decompile_fail_output_path(output_path)
-
-                output_path = decompile_base_apk_with_r(package_dir)
-
-                change_network_security_config_with_r(output_path)
-
-                recompile_base_apk(output_path, package_dir)
+                    recompile_merged_apk(output_path, package_dir)
 
 
             # APK 파일 서명
             sign_apks(package_dir)
 
+            #안드로이드 내 앱 삭제
+            remove_app(package_name)
+
             # 서명된 APK 파일 설치
-            install_signed_apks(package_name, package_dir)
+            install_signed_apks(package_dir)
 
             
             excelFunc.excelEndPoint(startPoint, endPoint, app_name, None, None, None)
@@ -415,7 +474,7 @@ def main():
             
             excelFunc.excelEndPoint(startPoint, endPoint, app_name, result, error, str(e))
             # subprocess.run(['adb', 'uninstall', package_name], check=True)
-            print(f"{package_name} 패키지 삭제 완료.")
+            # print(f"{package_name} 패키지 삭제 완료.")
 
 
     sys.exit(0)  # 프로그램 종료
--- excelFunc.py
+++ excelFunc.py
@@ -20,13 +20,17 @@ def excelEndPoint(startPoint, endPoint, app_name, result, error, detail):
     wb = openpyxl.load_workbook(fpath)
 
     ws = wb['Sheet1']
-    last_row = ws.max_row
-
-    for row in range(startPoint, endPoint, 1):
+    
+    # startPoint와 endPoint가 올바른 범위인지 확인
+    if startPoint > endPoint:
+        raise ValueError("startPoint는 endPoint보다 작거나 같아야 합니다.")
+    
+    for row in range(startPoint, endPoint + 1):
+        # 각 반복에서 새로운 마지막 행을 찾습니다.
+        last_row = ws.max_row
         ws[f'A{last_row + 1}'] = app_name
         ws[f'B{last_row + 1}'] = result
         ws[f'C{last_row + 1}'] = error
         ws[f'D{last_row + 1}'] = detail
-
-
+        
     wb.save(fpath)
